4387ec43755b16c5efe879bea663dba1397a30e1
Erick Bellido <erick@MacBook-Air-de-Erick.local>
2025-11-28 20:39:07 -0500

    solucion de errores 2

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import os
 from fastapi import FastAPI
-from app.routers import risk_router, telemetry_router
 from dotenv import load_dotenv
 from app.services.mqtt_service import iniciar_servicio_mqtt
 from app.services.thingsboard_service import iniciar_conexion_tb
