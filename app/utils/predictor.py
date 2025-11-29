112b4195fc888d2ea10f1f7fa64f8274b93bffc7
Erick Bellido <erick@MacBook-Air-de-Erick.local>
2025-11-29 02:58:05 -0500

    depuracion

--- app/utils/predictor.py
+++ app/utils/predictor.py
@@ -20,7 +20,7 @@ def process_message(esp_id, payload):
         return {"error": f"Falta el campo {e}"}
 
     # Vector ordenado tal como fue durante el entrenamiento
-    X = np.array([[temperatura, humo, llama]],dtype=float)
+    X = np.array([[temperatura, humo, llama]],columns=["temperatura", "humo", "llama"])
 
     # ============================
     # 2. Obtener modelo correcto
